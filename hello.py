a0a865bb9c5539a05d60174eb994ab5ea18e104a
vinodbavage31 <bavagevinod604@gmail.com>
2025-01-28 17:08:19 +0530

    sets and dictionary

--- hello.py
+++ hello.py
@@ -306,9 +306,171 @@ Grade students based on marks
 
 #WAP to ask the user to enter the names of three movies and store them in a list
 
-movie1 = list[input("enter the 1st movie name:")]
-movie2 = list[input("enter the 2nd movie name:")]
-movie3 = list[input("enter the 3rd movie name:")]
-print(movie1, movie2, movie3)
+# movies = []
+# movie1 = input("Enter 1st movie name :")
+# movie2 = input("Enter 2nd movie name :")
+# movie3 = input("Enter 3rd movie name :")
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
+# print("the movies list is :", movies)
 
 
+# movies = []
+# movies.append(input("enter 1st movie : "))
+# movies.append(input("enter 2nd movie : "))
+# movies.append(input("enter 3rd movie : "))
+# print("the movie list is", movies)
+
+
+
+
+# WAP to check weather the number is palindrome or not
+# list1 = [4,5,3,6,9,7]
+# list1_copy = list1.copy()
+# list1_copy.reverse()
+
+# if(list1_copy == list1):
+#   print("The number is palindrome:")
+# else:
+#   print("Not palindrome :")
+
+
+#WAP to count the number of students with the a grade in the following tuple
+#["c", "d", "a", "a", "b", "b","a"]
+
+# grade = ("c", "d", "a", "a", "b", "b","a")
+# print(grade.count("a"))
+
+
+
+#WAP to sort the following list of items from a to d   
+# lst = ["c", "d", "a", "a", "b", "b","a"]
+# lst.sort()
+# print(lst)
+# ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#Dictionary
+
+# info = {
+#   "name" : "vinod",
+#   "studing" : "be",
+#   "sub" : ["hindi", "english"],
+#   "topic" : ("tuple",),
+#   "age": 22,
+#   "marks":99,
+#   "cgpa": 9.9,
+# }
+# print(info)
+# print("name",info["name"])
+# print(info["sub"])
+# print(info["topic"])
+# print(info["age"])
+# print(info["cgpa"])
+# print(info["name"])
+
+#nested dictionary
+
+# student = {
+#   "name" : "vinod",
+#   "class" : "BE final",
+#   "marks" : {
+#     "mat" : 98,
+#     "phy": 99,
+#     "chem" : 89,
+#     "ele" : 94,
+#    }
+# }
+# print(student)
+# print("the student name is :",student["name"])
+# print("Student marks with subjec ts:",student ["marks"])
+# print(student ["marks"] ["phy"])
+# print(list(student.keys()))
+# print(list(student.values()))
+# print(list(student.items()))
+# print(student.get("name"))
+# pairs = list(student.items())
+# print(pairs[1])
+# print(len(student.values()))
+
+# print(student["name"])
+# print(student.get("name"))
+# new_det = {"city":"bhalki"}
+# student.update(new_det)
+# print(student)
+
+#sets in python
+
+# collection = {1, 2, 5, 88, "vinod", "hello", "world"}
+# print(collection)
+# print(len(collection))
+# print(type(collection))
+
+# collection = set() #empty set (syntax of empty set)
+# print(type(collection))
+
+#methods of sets
+
+# collection = set()
+# collection.add(2)
+# collection.add(4567)
+# collection.add(9)
+# collection.remove(2)
+# collection.remove(9)
+# collection.remove()
+# collection.add(5)
+# print(collection)
+
+
+# set1 = {2, 9, 5, 4, 8, 96, 48, 67, 25}
+# set2 = {2, 9, 5,84,56, 48, 67, 25}
+
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# collection.remove(2)
+# collection.remove(9)
+# collection.remove(5)
+# print(collection)
+
+#WAP to store following word meaning in python dictionary
+# home = {
+#   "table" : ["furniture", "list of facts and figures"],
+#   "cat" : "a small animal",
+# }
+# print(home)
+
+#WAP to count how many class rooms are required for following subjects. if each subject is taking one classroom
+# class1 = {"python", "java", "c++", "python", "javascript", "java", "python", "java", "c", "c++"}
+# print("the number of classromm needed for all the students is :",len(class1))
+"""
+WAP to enter marks of 3 subjects from the user andstore them in a dictionary. starts with an empty dictionary and add one by on, 
+use subjects as keys and marks as values"""
+
+
+# student = {}
+# x = int(input("enter che marks :"))
+# student.update({"che":x})
+
+# x = int(input("enter maths marks :"))
+# student.update({"maths":x})
+
+# x = int(input("enter phy marks :"))
+# student.update({"phy":x})
+
+# print(student)
+
+
+#WAP to store 9 and 9.0 as supparate values in the set
+
+# val = set()
+# val.add(9)
+# val.(9.0)
+# print(val)
+
+val = {
+  ("float",9.0),
+   ("int",9),
+   }
+print(val)
